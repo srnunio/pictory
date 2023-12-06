98bce6e68a8e0520a641120463e01d7b21e8a6fb
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2023-12-06 16:12:36 +0100

    improvements of get view photo

--- Pictory/View/Photo/GetPhotoView.swift
+++ Pictory/View/Photo/GetPhotoView.swift
@@ -6,13 +6,102 @@
 //
 
 import SwiftUI
+import CachedAsyncImage
 
 struct GetPhotoView: View {
+    var id: Int
+    var namespace: Namespace.ID
+    @Binding var showed: Bool
+    @StateObject var model = GetPhotoViewModel()
+    
+    var onDelete: (() -> Void)?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack(spacing: 0) {
+                HStack {
+                    ButtonAction(systemName: "plus") { onClose() }
+                        .rotationEffect(.init(degrees: 45))
+                    Spacer()
+                    ButtonAction(systemName: "trash") { onDelete?() }
+                        .padding(.horizontal, 16.0)
+                }
+                .foregroundColor(.primary)
+            }
+            .frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity,
+                alignment: .top
+            )
+            
+            VStack {
+                if model.isBusy {
+                    ProgressView()
+                }
+                else if model.hasError {
+                    MessageView(message: labelOfError())
+                }
+                else if !model.hasData {
+                    MessageView(message: "notfound".toTranslate)
+                }else {
+                    ImageView(photo: model.photo)
+                }
+            } 
+            .onAppear {
+                model.load(id: id)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func ImageView(photo: PexelPhoto) -> some View {
+        VStack(spacing: 0) {
+            CachedAsyncImage(
+                url: photo.originalImage,
+                placeholder: {
+                    ProgressView()
+                },
+                image: {data in
+                    Image(cpImage: data)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(10)
+                        .frame(
+                            maxWidth: CGFloat( photo.width),
+                            maxHeight: CGFloat( photo.height))
+                        
+                },
+                error: { error in
+                    MessageView(message: error)
+                }
+            )
+            .matchedGeometryEffect(id: "\(photo.id)", in: namespace)
+            .padding()
+            .onTapGesture { onClose() }
+        }
+    }
+    
+    
+    private func onClose() {
+        withAnimation { showed = false }
+    }
+    
+    func labelOfError() -> String {
+        
+        if !model.hasError { return "" }
+        
+        switch model.error {
+        case .unauthorized:
+            return "unauthorized".toTranslate
+        case .notfound:
+            return "notfound".toTranslate
+        case .unknown:
+            return "failure".toTranslate
+        }
     }
 }
 
 #Preview {
-    GetPhotoView()
+    @Namespace var namespace
+    return GetPhotoView(id: 2014422,namespace: namespace, showed: .constant(true))
 }
