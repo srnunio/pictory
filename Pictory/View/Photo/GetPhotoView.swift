40fa3c168e3a7a4ea76cc1c95efe46251a0da3bc
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2023-12-06 16:04:42 +0100

    add custom button view

---
+++ Pictory/View/Photo/GetPhotoView.swift
@@ -0,0 +1,18 @@
+//
+//  GetPhotoView.swift
+//  Pictory
+//
+//  Created by Edvaldo Martins on 06/12/2023.
+//
+
+import SwiftUI
+
+struct GetPhotoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GetPhotoView()
+}
