00a43510ddbc71da48bfb847cb27a0322807fb2d
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2023-12-12 12:19:33 +0100

    fixes

--- Pictory/Helpers/Constants.swift
+++ Pictory/Helpers/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 final class Constants {
     static let api = "https://api.pexels.com"
-    static let apiKey = "vkQ0JMyvFE70WZMFXuHZ4Z8g6y3XUuYSxC3EGglsL1ihoiMoVy3mqZE3"
+    static let apiKey = "KEY_API"
     static let perPage = 80
     static let dataBaseName = "pictory"
     static let defaultLocale = "pt_PT"
